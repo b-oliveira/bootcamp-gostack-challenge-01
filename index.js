ea02451a0a49b9167b8281a54a936357de882927
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-06-29 17:41:34 -0300

    feat: Implementando endpoint que cria um projeto

--- index.js
+++ index.js
@@ -3,4 +3,24 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+
+const projects = [];
+
+/**
+ * Cria um projeto.
+ */
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
 server.listen(3000);
